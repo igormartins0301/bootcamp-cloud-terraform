e990308c08c0ccc1d222469b9a640a47c50a33c2
Igor Martins <igormartins0301@gmail.com>
2025-03-09 21:09:00 -0300

    feat: incluindo primeiro arquivo tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,4 @@
-# terraform {
-#   required_version = ">= 1.0"
-
-#   backend "s3" {
-#     bucket = "nome-do-seu-bucket-terraform"
-#     key    = "terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
-
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
